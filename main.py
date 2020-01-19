ba55f2c3a86ba352cfae78d520c414c512c3d1ba
EsmaeilEshaghi <Es_eshaghi@yahoo.com>
2020-01-20 10:36:24 +1100

    Conflics are solved

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ print ('hello world')
 print ("I'll be back")
 
 for i in range(10):
-<<<<<<< HEAD
+
 #What are you doing buddy
-=======
 #I am running a test
->>>>>>> refs/remotes/origin/master
+
     print (i)
