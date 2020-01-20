c16421953c594247610773898306862600bb0049
EsmaeilEshaghi <Es_eshaghi@yahoo.com>
2020-01-20 11:19:16 +1100

    New branch

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ print ('hello world')
 print ("I'll be back")
 
 for i in range(10):
-
-#What are you doing buddy
-#I am running a test
+# Another comment...
+    #What are you doing buddy
+    #I am running a test
 
     print (i)
+
+print ('Github it is me')
